a81b00e0eda018b7f6111db70603e2de159363a3
diogo262 <thenolance10@gmail.com>
2025-04-13 21:00:03 -0300

    feat: design de formulário de login

---
+++ NEGOCIOS/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associada a um assembly.
+[assembly: AssemblyTitle("NEGOCIOS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NEGOCIOS")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("dc5d183c-a00b-4f6b-a0c1-f5896384159e")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SistemaControleFinanceiro/login.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace SistemaControleFinanceiro
+{
+    partial class login
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(login));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.tbx_usuario = new System.Windows.Forms.TextBox();
+            this.tbx_senha = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.LBL_CRIARCONTA = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Franklin Gothic Book", 12.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(75, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(302, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SISTEMA DE CONTROLE FINANCEIRO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Bahnschrift", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(65, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(180, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nome de usuário ou Email:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Bahnschrift", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(65, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Senha:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(389, -15);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(89, 96);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(-19, 272);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(93, 110);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // tbx_usuario
+            // 
+            this.tbx_usuario.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.tbx_usuario.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbx_usuario.ForeColor = System.Drawing.SystemColors.Control;
+            this.tbx_usuario.Location = new System.Drawing.Point(68, 114);
+            this.tbx_usuario.Margin = new System.Windows.Forms.Padding(0);
+            this.tbx_usuario.Name = "tbx_usuario";
+            this.tbx_usuario.Size = new System.Drawing.Size(309, 20);
+            this.tbx_usuario.TabIndex = 5;
+            this.tbx_usuario.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // tbx_senha
+            // 
+            this.tbx_senha.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.tbx_senha.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbx_senha.ForeColor = System.Drawing.SystemColors.Control;
+            this.tbx_senha.Location = new System.Drawing.Point(68, 179);
+            this.tbx_senha.Margin = new System.Windows.Forms.Padding(0);
+            this.tbx_senha.Name = "tbx_senha";
+            this.tbx_senha.Size = new System.Drawing.Size(309, 20);
+            this.tbx_senha.TabIndex = 6;
+            this.tbx_senha.UseSystemPasswordChar = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Bahnschrift", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(151, 272);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(155, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Não possui uma conta?";
+            // 
+            // LBL_CRIARCONTA
+            // 
+            this.LBL_CRIARCONTA.AutoSize = true;
+            this.LBL_CRIARCONTA.BackColor = System.Drawing.Color.Transparent;
+            this.LBL_CRIARCONTA.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.LBL_CRIARCONTA.Font = new System.Drawing.Font("Bahnschrift", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBL_CRIARCONTA.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.LBL_CRIARCONTA.Location = new System.Drawing.Point(193, 289);
+            this.LBL_CRIARCONTA.Name = "LBL_CRIARCONTA";
+            this.LBL_CRIARCONTA.Size = new System.Drawing.Size(70, 17);
+            this.LBL_CRIARCONTA.TabIndex = 8;
+            this.LBL_CRIARCONTA.Text = "Crie uma!";
+            this.LBL_CRIARCONTA.Click += new System.EventHandler(this.LBL_CRIARCONTA_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(184, 230);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 25);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Entrar";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(450, 356);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.LBL_CRIARCONTA);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbx_senha);
+            this.Controls.Add(this.tbx_usuario);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "login";
+            this.ShowIcon = false;
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox tbx_usuario;
+        private System.Windows.Forms.TextBox tbx_senha;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label LBL_CRIARCONTA;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
